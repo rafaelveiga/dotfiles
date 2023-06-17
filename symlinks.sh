6388624f5a1f69f00e45387ef9d24b1040c579e7
Rafael Veiga <dsg.rafael@gmail.com>
2023-06-17 20:10:47 -0300

    init bspwm

---
+++ symlinks.sh
@@ -0,0 +1,14 @@
+# symlink bspwm
+ln -s /media/sf_dotfiles/config/bspwm ~/.config/bspwm
+
+# symlink sxhkd
+ln -s /media/sf_dotfiles/config/sxhkd ~/.config/sxhkd
+
+# symlink polybar
+ln -s /media/sf_dotfiles/config/polybar ~/.config/polybar
+
+# symlink .xinitrc
+ln -s /media/sf_dotfiles/.xinitrc ~/.xinitrc
+
+# symlink picom
+ln -s /media/sf_dotfiles/config/picom ~/.config/picom
