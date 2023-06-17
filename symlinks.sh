c812a0abf40e99aabc774b4ef576ebc637d7385b
Rafael Veiga <dsg.rafael@gmail.com>
2023-06-17 20:50:05 -0300

    nitrogen, install, polybar, rofi, picom

--- symlinks.sh
+++ symlinks.sh
@@ -12,3 +12,12 @@ ln -s /media/sf_dotfiles/.xinitrc ~/.xinitrc
 
 # symlink picom
 ln -s /media/sf_dotfiles/config/picom ~/.config/picom
+
+# symlink wallpapers to .wallpapers
+ln -s /media/sf_dotfiles/wallpapers ~/.wallpapers
+
+# symlink nitrogen
+ln -s /media/sf_dotfiles/config/nitrogen ~/.config/nitrogen
+
+# symlink rofi
+ln -s /media/sf_dotfiles/config/rofi ~/.config/rofi
