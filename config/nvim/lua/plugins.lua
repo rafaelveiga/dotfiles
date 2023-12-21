6792a265e3f136ac9eb4eb57e1786ff16fc85070
Rafael Veiga <dsg.rafael@gmail.com>
2023-12-20 23:46:10 -0300

    move plugin config to its own file

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -67,75 +67,16 @@ require("lazy").setup(plugins, opts)
 require("bufferline").setup{}
 
 -- status line
-require('lualine').setup {
-  options = {
-    theme = 'dracula-nvim'
-  }
-}
+require("plugins.lualine");
 
 -- lsp setup
-local lspconfig = require("lspconfig")
-lspconfig.tsserver.setup {}
+require("plugins.lsp")
 
 -- mason setup
-local mason = require("mason")
-mason.setup()
-
--- null ls setup
+require("plugins.mason");
 
 -- prettier setup
-local prettier = require("prettier")
-
-prettier.setup({
-  bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
-  filetypes = {
-    "css",
-    "graphql",
-    "html",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "less",
-    "markdown",
-    "scss",
-    "typescript",
-    "typescriptreact",
-    "yaml",
-  },
-})
+require("plugins.prettier");
 
 -- autocompletion setup
-local status, cmp = pcall(require, "cmp")
-if (not status) then return end
-local lspkind = require 'lspkind'
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true
-    }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-  }),
-  formatting = {
-    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
-  }
-})
-
-vim.cmd [[
-  set completeopt=menuone,noinsert,noselect
-  highlight! default link CmpItemKind CmpItemMenuDefault
-]]
-
+require("plugins.cmp")
---
+++ config/nvim/lua/plugins/prettier.lua
@@ -0,0 +1,21 @@
+-- prettier setup
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+})
+
