f4f077d65ce54b5607b520896ef887835d133c83
Rafael Veiga <dsg.rafael@gmail.com>
2024-05-22 22:58:10 -0300

    restart nvim

---
+++ config/nvim/lua/set.lua
@@ -0,0 +1,17 @@
+-- relative line numbers
+vim.opt.relativenumber = true
+
+-- Maps the leader key to space
+vim.g.mapleader = " "
+
+-- Sets the tab size to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- Sets color scheme
+-- vim.cmd('colorscheme dracula')
+
+-- If 300ms have passed while nothing is typed, the swap file will be written do disk
+vim.opt.updatetime = 300
